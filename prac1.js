a1fa6a3a3221eacb5b5960c24a6c1926d508babc
Amanpreet-2004 <kaurcsamanpreet@gmail.com>
2024-10-07 06:21:21 +0530

    practice

---
+++ prac1.js
@@ -0,0 +1,12 @@
+//splice
+
+let arr = [1, 2, 3, 4, 5,6,7,8,9];
+arr.splice(1, 5);  
+console.log(arr);
+
+//slice
+
+let arr1=[12,34,56,78,11,10,18]
+let newArr =arr1.slice(3,6)
+console.log(arr1)
+console.log(newArr)
