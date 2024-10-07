8936c74eb15ea480cd4a9e9f3c6c22a56570f0fe
Amanpreet-2004 <kaurcsamanpreet@gmail.com>
2024-10-08 05:22:35 +0530

    practice

--- prac1.js
+++ prac1.js
@@ -10,3 +10,28 @@ let arr1=[12,34,56,78,11,10,18]
 let newArr =arr1.slice(3,6)
 console.log(arr1)
 console.log(newArr)
+
+//arrow functions
+
+const val=(a,b)=>a+b
+console.log(val(3,8))
+
+//normal function
+
+let func=function(a,b,c){
+    return a*b*c;
+}
+console.log(func(2,4,7))
+
+//map
+
+const Arr = [1,2,2,3,4,5,6,6,6,7,7,8];
+const mp=new Map();
+Arr.forEach((word)=>{
+    if (mp.has(word)){
+mp.set(word,mp.get(word)+1);
+    }else{
+        mp.set(word,1)
+    }
+})
+console.log("mp",mp)
